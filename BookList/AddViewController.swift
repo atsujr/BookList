6155527744276d334c728c5c2c54de5ab95ccfef
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-09-24 19:14:44 +0900

    データを保存して、tableviewに表示するまで

--- BookList/AddViewController.swift
+++ BookList/AddViewController.swift
@@ -5,57 +5,233 @@
 //  Created by Atsuhiro Muroyama on 2022/09/19.
 //
 
+import Foundation
 import UIKit
+import RealmSwift
 
-class AddViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+class AddViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
     
-    @IBOutlet weak var bookImageButton: UIButton!
-    // @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookImage: UIImageView!
     @IBOutlet weak var bookTitleTextField: UITextField!
     @IBOutlet weak var autherTextField: UITextField!
     @IBOutlet weak var oneOfThreeWords: UITextField!
     @IBOutlet weak var twoOfThreeWords: UITextField!
     @IBOutlet weak var threeOfThreeWords: UITextField!
     
-    @IBOutlet weak var bookmemo: UITextField!
+    @IBOutlet weak var bookPointLabel: UILabel!
+    
+    let realm = try! Realm()
+    
+    @IBOutlet weak var bookmemoTextField: UITextField!
+    
+    var sliderNum: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        makeBookTitleToolButton()
+        makeAutherToolButton()
+        makeOneOfThreeToolButton()
+        maketwoOfThreeToolButton()
+        makethreeOfThreeToolButton()
         // Do any additional setup after loading the view.
+        bookPointLabel.text = String(sliderNum)
     }
-//    @IBAction func takePhoto(_ sender: Any) {
-//        presentPickerController(sourceType: .camera)
-//    }
-    @IBAction func save(_ sender: Any) {
-    }
-    @IBAction func tapimagebutton(_ sender: Any) {
+    @IBAction func takePhoto(_ sender: Any) {
         presentPickerController(sourceType: .camera)
     }
+    @IBAction func save(_ sender: Any) {
+        saveTweet()
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func sliderchancge(_ sender: UISlider) {
+        //sliderNum = String(format: "%.0f", sender.value * 100)
+        bookPointLabel.text = String(format: "%.0f", sender.value * 100)
+        sliderNum = Int(sender.value * 100)
+    }
+    
+    func makeBookTitleToolButton(){
+        let toolbar = UIToolbar()
+        
+        //完了ボタンを右寄せにする為に、左側を埋めるスペース作成
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                    target: nil,
+                                    action: nil)
+        //完了ボタンを作成
+        let done = UIBarButtonItem(title: "完了",
+                                   style: .done,
+                                   target: self,
+                                   action: #selector(didTapBookTitleDoneButton))
+        toolbar.items = [space, done]
+        toolbar.sizeToFit()
+        
+        bookTitleTextField.inputAccessoryView = toolbar
+    }
+    @objc func didTapBookTitleDoneButton() {
+        bookTitleTextField.resignFirstResponder()
+    }
+    func makeAutherToolButton(){
+        let toolbar = UIToolbar()
+        
+        //完了ボタンを右寄せにする為に、左側を埋めるスペース作成
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                    target: nil,
+                                    action: nil)
+        //完了ボタンを作成
+        let done = UIBarButtonItem(title: "完了",
+                                   style: .done,
+                                   target: self,
+                                   action: #selector(didTapAutherDoneButton))
+        toolbar.items = [space, done]
+        toolbar.sizeToFit()
+        
+        autherTextField.inputAccessoryView = toolbar
+    }
+    @objc func didTapAutherDoneButton() {
+        autherTextField.resignFirstResponder()
+    }
+    func makeOneOfThreeToolButton(){
+        let toolbar = UIToolbar()
+        
+        //完了ボタンを右寄せにする為に、左側を埋めるスペース作成
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                    target: nil,
+                                    action: nil)
+        //完了ボタンを作成
+        let done = UIBarButtonItem(title: "完了",
+                                   style: .done,
+                                   target: self,
+                                   action: #selector(didTapOneOfThreeDoneButton))
+        toolbar.items = [space, done]
+        toolbar.sizeToFit()
+        
+        oneOfThreeWords.inputAccessoryView = toolbar
+    }
+    @objc func didTapOneOfThreeDoneButton() {
+        oneOfThreeWords.resignFirstResponder()
+    }
+    func maketwoOfThreeToolButton(){
+        let toolbar = UIToolbar()
+        
+        //完了ボタンを右寄せにする為に、左側を埋めるスペース作成
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                    target: nil,
+                                    action: nil)
+        //完了ボタンを作成
+        let done = UIBarButtonItem(title: "完了",
+                                   style: .done,
+                                   target: self,
+                                   action: #selector(didTaptwoOfThreeDoneButton))
+        toolbar.items = [space, done]
+        toolbar.sizeToFit()
+        
+        twoOfThreeWords.inputAccessoryView = toolbar
+    }
+    @objc func didTaptwoOfThreeDoneButton() {
+        twoOfThreeWords.resignFirstResponder()
+    }
+    func makethreeOfThreeToolButton(){
+        let toolbar = UIToolbar()
+        
+        //完了ボタンを右寄せにする為に、左側を埋めるスペース作成
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                    target: nil,
+                                    action: nil)
+        //完了ボタンを作成
+        let done = UIBarButtonItem(title: "完了",
+                                   style: .done,
+                                   target: self,
+                                   action: #selector(didTapthreeOfThreeDoneButton))
+        toolbar.items = [space, done]
+        toolbar.sizeToFit()
+        
+        oneOfThreeWords.inputAccessoryView = toolbar
+    }
+    @objc func didTapthreeOfThreeDoneButton() {
+        threeOfThreeWords.resignFirstResponder()
+    }
+    
+    
+
+    
+    
+    func saveTweet() {
+        guard let bookTitle = bookTitleTextField.text else { return }
+        guard let authertf = autherTextField.text else { return }
+        
+        guard let oneOfThree = oneOfThreeWords.text else { return }
+        guard let twoOfThree = twoOfThreeWords.text else { return }
+        guard let threeOfThree = threeOfThreeWords.text else { return }
+        
+        //guard let bookreviewpoint = sliderNum else { return }
+        
+        guard let bookmemo = bookmemoTextField.text else { return }
+        
+        let booklog = BookLog()
+        
+        booklog.bookName = bookTitle
+        booklog.auther = authertf
+        //読んだ日時を取得
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        booklog.readTime = formatter.string(from: date)
+        
+        booklog.bookPoint = sliderNum
+        
+        booklog.oneOfThreeWords = oneOfThree
+        booklog.twoOfThreeWords = twoOfThree
+        booklog.threeOfThreeWords = threeOfThree
+        booklog.memo = bookmemo
+        
+        if let settingbookimage = bookImage.image{
+            let bookimageurl = saveImage(image: settingbookimage)
+            
+            booklog.bookImageFileName = bookimageurl
+        }
+        try! realm.write({
+            realm.add(booklog) // レコードを追加している
+        })
+        
+        
+        
+    }
+    // 画像を保存するメソッド
+    func saveImage(image: UIImage) -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else { return nil }
+        
+        do {
+            let fileName = UUID().uuidString + ".jpeg" // ファイル名を決定(UUIDは、ユニークなID)
+            let imageURL = getImageURL(fileName: fileName) // 保存先のURLをゲット
+            try imageData.write(to: imageURL) // imageURLに画像を書き込む
+            return fileName
+        } catch {
+            print("Failed to save the image:", error)
+            return nil
+        }
+    }
+    
+    // URLを取得するメソッド
+    func getImageURL(fileName: String) -> URL {
+        let docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return docDir.appendingPathComponent(fileName)
+    }
+    
+    
+    
     func presentPickerController(sourceType: UIImagePickerController.SourceType){
         if UIImagePickerController.isSourceTypeAvailable(sourceType){
             //上のif文章は、カメラが使用可能か調べている。
-        let picker = UIImagePickerController()
-        picker.sourceType = sourceType
-        picker.delegate = self
-        self.present(picker, animated: true, completion: nil)
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
         }
     }
     func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
-        
-        //self.dismiss(animated: true, completion: nil)
-        let pickerImage = info[.originalImage] as? UIImage
-        bookImageButton.setBackgroundImage(pickerImage, for: .normal)
-        picker.dismiss(animated: true)
+        self.dismiss(animated: true, completion: nil)
+        bookImage.image = info[.originalImage] as? UIImage
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
--- BookList/ViewController.swift
+++ BookList/ViewController.swift
@@ -6,36 +6,79 @@
 //
 
 import UIKit
+import RealmSwift
 
 class BookListViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableview: UITableView!
+    
+    let realm = try! Realm()
+    
+    var booklogs = [BookLog]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableview.register(UINib(nibName: "BookTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         tableview.dataSource = self
         tableview.delegate = self
-        
+        getbooklistData()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getbooklistData()
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         let screenRect = UIScreen.main.bounds
         tableview.frame = CGRect(x: 0, y: 0, width: screenRect.width, height: screenRect.height)
     }
+    func getbooklistData() {
+        booklogs = Array(realm.objects(BookLog.self)).reversed()
+        tableview.reloadData()
+    }
     
+    //ここから下はtabkleview周り
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return booklogs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! BookTableViewCell
-        cell.booknameLabel.text = "ばなな"
-        cell.bookImage.image = UIImage(systemName: "swift")
+        
+        let booklog = booklogs[indexPath.row]
+        cell.booknameLabel.text = booklog.bookName
+        cell.writerLabel.text = booklog.auther
+        cell.readingDayLabel.text = booklog.readTime
+        cell.oneOfThreeWordsLabel.text = booklog.oneOfThreeWords
+        cell.twoOfThreeWordsLabel.text = booklog.twoOfThreeWords
+        cell.threeOfWordsLabel.text = booklog.threeOfThreeWords
+        
+        if let imageFileName = booklog.bookImageFileName {
+            let path = getImageURL(fileName: imageFileName).path // 画像のパスを取得
+            if FileManager.default.fileExists(atPath: path) { // pathにファイルが存在しているかチェック
+                if let imageData = UIImage(contentsOfFile: path) { // pathに保存されている画像を取得
+                    cell.bookImage.image = imageData
+                } else {
+                    print("Failed to load the image. path = ", path)
+                }
+            } else {
+                print("Image file not found. path = ", path)
+            }
+        }
         return cell
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 156
     }
+    //urlを取得
+    func getImageURL(fileName: String) -> URL {
+        let docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return docDir.appendingPathComponent(fileName)
+    }
+    
 }
